58602b8e671c14bd6203c591a10596f6b4daac30
HQ <cr646531@gmail.com>
2022-03-23 22:03:56 -0400

    setting up bare bones functionality

---
+++ src/config/aliases.js
@@ -0,0 +1,7 @@
+const aliases = (prefix = `src`) => ({
+    '@components': `${prefix}/components`,
+    '@config': `${prefix}/config`,
+    '@constants': `${prefix}/constants`,
+});
+
+module.exports = aliases;
