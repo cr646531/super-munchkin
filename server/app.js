1297fd3b1c2722c43a2c2193b89d1ffadfba13ab
HQ <cr646531@gmail.com>
2022-04-02 03:34:39 -0400

    set turn order and current player based on turn

--- server/app.js
+++ server/app.js
@@ -17,12 +17,34 @@ app.get('/', (req, res, next) => res.sendFile(indexFile));
 
 app.get('/data/init', async (req, res, next) => {
     try {
-        const player = await Player.findOne({ where: { name: 'Buckets' } });
+        let player = await Player.findOne({ where: { status: 'active' } });
+        let players = await Player.findAll({ order: conn.random() });
+
+        // game start - set the turn order
+        if (!player) {
+            for (let i = 0; i < players.length; i++) {
+                if (i === 0) {
+                    players[0].status = 'active';
+                    await players[0].save();
+                }
+                if (i === players.length - 1) {
+                    players[i].setPlayer(players[0]);
+                    await players[i].save();
+                } else {
+                    players[i].setPlayer(players[i + 1]);
+                    await players[i].save();
+                }
+            }
+
+            player = await Player.findOne({ where: { status: 'active' } });
+            players = await Player.findAll();
+        }
+
         const hand = await Card.findAll({ where: { PlayerId: player.id } });
         const doors = await Card.findAll({ where: { type: 'door', status: 'inactive', PlayerId: null } });
         const treasures = await Card.findAll({ where: { type: 'treasure', status: 'inactive' } });
         const active = await Card.findOne({ where: { status: 'active' }, order: conn.random(), limit: 1 });
-        res.send({ player, hand, doors, treasures, active });
+        res.send({ player, players, hand, doors, treasures, active });
     } catch (err) {
         console.log('err: ', err);
         next(err);
