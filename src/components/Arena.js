877909404282f16b5f48f0578b6024190a845301
HQ <cr646531@gmail.com>
2022-04-02 08:38:39 -0400

    cleaned up Arena to make it more readable and DRY

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -1,99 +1,58 @@
-import React, { Component } from 'react';
-import { Button, Drawer, Typography } from '@mui/material';
-import { Card, Grid } from '@components';
+import React from 'react';
+import { Card } from '@components';
 import { getData, kickOpenDoor, lootTheRoom, cardUpdate, playerUpdate } from '../store';
 import { connect } from 'react-redux';
 
 const styles = {
-    row: {
+    root: {
+        height: '100%',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        flex: 1,
-        marginTop: 16,
-        marginBottom: 16,
-    },
-    col: {
-        display: 'flex',
-        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 };
 
 const Arena = ({ getData, active, doors, kickOpenDoor, lootTheRoom, player, treasures, cardUpdate, playerUpdate }) => {
+    const onClickDeck = () => {
+        if (player.phase === 'kick') {
+            kickOpenDoor();
+        } else if (player.phase === 'loot') {
+            lootTheRoom();
+            setTimeout(() => {
+                getData();
+            }, 1000);
+        }
+    };
+
+    const onClickActive = () => {
+        switch (active.category) {
+            case 'race' || 'class':
+                // put the card into the players hand
+                active.status = 'inactive';
+                active.PlayerId = player.id;
+                cardUpdate(active);
+
+                // progress to 'Loot the Room' stage
+                player.phase = 'loot';
+                playerUpdate(player);
+                setTimeout(() => getData(), 300);
+        }
+    };
+
     return (
-        <div
-            style={{
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <div>
-                <Card small onClick={() => console.log('graveyard')} style={{ marginBottom: 16 }} />
-                {doors.length ? (
-                    <Card
-                        card={{
-                            type: 'door',
-                        }}
-                        face='down'
-                        small
-                        onClick={() => {
-                            if (player.phase === 'kick') {
-                                kickOpenDoor();
-                            } else if (player.phase === 'loot') {
-                                lootTheRoom();
-                                setTimeout(() => {
-                                    getData();
-                                }, 1000);
-                            }
-                        }}
-                    />
-                ) : (
-                    <div />
-                )}
+        <div style={styles.root}>
+            <div style={{ marginRight: 48 }}>
+                <Card small style={{ marginBottom: 16 }} />
+                {doors.length ? <Card card={{ type: 'door' }} face='down' small onClick={onClickDeck} /> : <div />}
             </div>
-            <div style={{ width: 48 }} />
             <div>
                 <Card small onClick={() => console.log('graveyard')} style={{ marginBottom: 16 }} />
                 {treasures.length ? <Card card={{ type: 'treasure' }} face='down' small /> : <div />}
             </div>
 
             {active ? (
-                <Card
-                    card={active}
-                    medium
-                    onClick={() => {
-                        switch (active.category) {
-                            case 'race':
-                                // put the card into the players hand
-                                active.status = 'inactive';
-                                active.PlayerId = player.id;
-                                cardUpdate(active);
-
-                                // progress to 'Loot the Room' stage
-                                player.phase = 'loot';
-                                playerUpdate(player);
-                                setTimeout(() => {
-                                    getData();
-                                }, 300);
-                            case 'class':
-                                // put the card into the players hand
-                                active.status = 'inactive';
-                                active.PlayerId = player.id;
-                                cardUpdate(active);
-
-                                // progress to 'Loot the Room' stage
-                                player.phase = 'loot';
-                                playerUpdate(player);
-                                setTimeout(() => {
-                                    getData();
-                                }, 300);
-                        }
-                    }}
-                    style={{ marginLeft: 160 }}
-                />
+                <Card card={active} medium onClick={onClickActive} style={{ marginLeft: 160 }} />
             ) : (
                 <Card small style={{ marginLeft: 160 }} />
             )}
